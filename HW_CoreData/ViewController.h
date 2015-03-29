d9a5229438219a9ccba69ec318cb5b60d50192ac
Alexander <sharrikk@gmail.com>
2015-03-30 01:21:59 +0300

    init

---
+++ HW_CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW_CoreData
+//
+//  Created by Alexander on 30.03.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
